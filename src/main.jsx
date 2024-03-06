b8b552dce5203c5b198ddeb9187ea89310d33c01
JurekZela <122160360+JurekZela@users.noreply.github.com>
2024-03-06 18:31:47 +0100

    ad

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './components/App.jsx';
 import { GlobalStyle } from './Index-styled.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
